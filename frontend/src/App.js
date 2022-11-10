3a39c991e34ab3e5b66b6665868f5366f4251f27
Buvaneswaran S <99597201+Indherjith@users.noreply.github.com>
2022-11-10 13:07:15 +0530

    product
    product page for all 5 divisions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import WomensProducts from './Pages/Womens/WomensProducts';
+import MensProducts from './Pages/Mens/MensProducts';
+import KidsProducts from './Pages/Kids/KidsProducts';
+import ShoesBagsProducts from './Pages/Shoes&Bags/ShoesBagsProducts';
+import Beauty from './Pages/Beauty/Beauty';
 
 function App() {
   return (
     <div className="App">
       <WomensProducts/>
+      <MensProducts/>
+      <KidsProducts/>
+      <ShoesBagsProducts/>
+      <Beauty/>
     </div>
   );
 }
