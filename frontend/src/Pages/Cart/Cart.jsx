211d8d4dbd1be3943f9750f585e372e351084ff5
Buvaneswaran S <99597201+Indherjith@users.noreply.github.com>
2022-11-12 18:59:21 +0530

    carthandling

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import styles from "./Cart.module.css"
 import {Link} from "react-router-dom"
+import CartHelp from './CartHelp'
 const Cart = () => {
- 
+    const [ce,setC] = useState(0);
     const cartData = JSON.parse(localStorage.getItem("cart"))
-    
-
-   
-
-
+    const handleC = ()=>{
+        setC(ce+1);
+    }
   return (
     <>
     <div className={styles.navbar_cart}></div>   {/* import navbar at attch here */}
@@ -17,34 +16,18 @@ const Cart = () => {
         <h1>Your Shopping Basket</h1>
         </div>
      {/* product count div */}
-     <div className={styles.product_count}><span>1</span><span>Product</span></div> 
+     <div className={styles.product_count}><span>{cartData.length} </span><span>Product</span></div> 
      {/* all cart functionality div starts   */}
      <div className={styles.cart_main}>
         {/* mapping of data starts here */}
-        {cartData.map(item => (
-        <div className={styles.items_div}>
-         <div className={styles.item_container}>
-          <div className={styles.item_img_main}>
-            <div className={styles.item_img_1}><img src={item.img} alt=''/></div>
-            <div>
-               <div className={styles.item_des} >{item.productName}</div>
-               <div className={styles.item_price}>{item.price}</div>
-               <div className={styles.item_size}>
-                <span>Colour: {item.colour}</span><br></br>
-                <span>Size: {item.size}</span>
-               </div>
-            </div>
-          </div>
-          <div className={styles.item_dilivery}>Delivery inEnter Pincode above</div>
-          <div className={styles.item_btn}>
-            <div><button>Remove</button></div>
-            <div><button>Move to favourites</button></div>
-          </div>
-         </div>
-         
         
-        </div>
+        <div className={styles.items_div}> 
+    {cartData.map((item) => (
+         <CartHelp item={item} handleC={()=>handleC()} />
+         
         ))}
+        </div>
+        
         {/* mapping of data ends here */}
         {/* pin check div start */}
         <div className={styles.price_main}>
